3cdde7ebde201f5845c0be05abdc0cccf2560957
markyamhs <markyamhs@gmail.com>
2020-06-17 20:26:59 +0800

    homepage updated

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./homepage.style.scss";
+
+import Directory from "../../directory/directory.component";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
